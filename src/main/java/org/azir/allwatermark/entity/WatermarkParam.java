5b2b8c30b961315cdff70665c5deaafa217abace
AzirZsk <49592289+AzirZsk@users.noreply.github.com>
2022-11-11 13:42:53 +0000

    add watermark params

--- src/main/java/org/azir/allwatermark/entity/WatermarkParam.java
+++ src/main/java/org/azir/allwatermark/entity/WatermarkParam.java
@@ -1,8 +1,26 @@
 package org.azir.allwatermark.entity;
 
 /**
+ * watermark params
+ * 
  * @author zhangshukun
  * @date 2022/11/8
  */
 public class WatermarkParam {
+
+    /**
+     * watermark text.
+     */
+    private String text;
+
+    /**
+     * 0-1 range.
+     */
+    private float transparency;
+
+    /**
+     * 0-360 range.
+     */
+    private float inclination;
+
 }
