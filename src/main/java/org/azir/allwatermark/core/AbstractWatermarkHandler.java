bd908aea414593b3a82c07dedefd3f64c0ad5a7f
Azir <2581866471@qq.com>
2022-11-12 16:58:05 +0800

    feat:add pdfbox dependency and new pdf handler class

--- src/main/java/org/azir/allwatermark/core/AbstractWatermarkHandler.java
+++ src/main/java/org/azir/allwatermark/core/AbstractWatermarkHandler.java
@@ -1,11 +1,10 @@
 package org.azir.allwatermark.core;
 
 /**
- *
  * @author zhangshukun
  * @date 2022/11/8
  */
-public abstract class AbstractWatermarkHandler {
+public abstract class AbstractWatermarkHandler implements WatermarkHandler {
 
 
 }
