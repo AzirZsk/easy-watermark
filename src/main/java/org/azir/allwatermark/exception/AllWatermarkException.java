a4c7c144320157c60f87a3d3b0d3246414151f05
Azir <2581866471@qq.com>
2022-11-12 16:56:53 +0800

    feat:add all-watermark exception class.

---
+++ src/main/java/org/azir/allwatermark/exception/AllWatermarkException.java
@@ -0,0 +1,14 @@
+package org.azir.allwatermark.exception;
+
+import java.io.IOException;
+
+/**
+ * @author Azir
+ * @date 2022/11/12
+ */
+public class AllWatermarkException extends IOException {
+
+    public AllWatermarkException(String message) {
+        super(message);
+    }
+}
