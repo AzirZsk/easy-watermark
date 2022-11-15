50aca7c57f8e302c5b4fac9ef63fd94724b6c0a3
zhangshukun <2581866471@qq.com>
2022-11-15 13:41:14 +0800

    feat:add watermark illegal check.

--- src/main/java/org/azir/allwatermark/utils/WatermarkParamUtils.java
+++ src/main/java/org/azir/allwatermark/utils/WatermarkParamUtils.java
@@ -9,16 +9,24 @@ import org.azir.allwatermark.exception.WatermarkParamException;
  */
 public class WatermarkParamUtils {
 
+    /**
+     * Check watermark param illegal.
+     *
+     * @param param watermark param.
+     */
     public static void checkParam(WatermarkParam param) {
         if (param == null) {
-            throw new NullPointerException("watermark param is null");
+            throw new NullPointerException("Watermark param is null.");
         }
         if (param.getText() == null) {
-            throw new NullPointerException("watermark text is null");
+            throw new NullPointerException("Watermark text is null.");
+        }
+        if (param.getTransparency() < 0 || param.getTransparency() > 1) {
+            throw new IllegalArgumentException("Transparency should be more than 0 and less than 1.");
         }
         boolean isCenter = param.isHorizontalCenter() || param.isOverspread();
         if (param.isOverspread() && isCenter) {
-            throw new WatermarkParamException("overspread and center can not same time open");
+            throw new WatermarkParamException("Overspread and center can not same time open.");
         }
     }
 
