732758daa4d09f431b342b424bb8a0a3d8227699
Azir <2581866471@qq.com>
2023-02-19 17:11:41 +0800

    feat:add main class

---
+++ src/main/java/org/azir/allwatermark/core/EasyWatermark.java
@@ -0,0 +1,23 @@
+package org.azir.allwatermark.core;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * @author Azir
+ * @date 2023/02/19
+ */
+public class EasyWatermark {
+
+    public static WatermarkHandler load(File file) {
+        return null;
+    }
+
+    public static WatermarkHandler load(InputStream inputStream) {
+        return null;
+    }
+
+    public static WatermarkHandler load(byte[] bytes) {
+        return null;
+    }
+}
