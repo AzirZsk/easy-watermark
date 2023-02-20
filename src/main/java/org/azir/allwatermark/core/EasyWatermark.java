c94723d11901209aaab645ed582ea2978a0a7c2d
Azir <2581866471@qq.com>
2023-02-20 23:30:51 +0800

    feat:init main class

--- src/main/java/org/azir/allwatermark/core/EasyWatermark.java
+++ src/main/java/org/azir/allwatermark/core/EasyWatermark.java
@@ -1,6 +1,10 @@
 package org.azir.allwatermark.core;
 
+import org.azir.allwatermark.exception.LoadFileException;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -9,14 +13,44 @@ import java.io.InputStream;
  */
 public class EasyWatermark {
 
-    public static WatermarkHandler load(File file) {
-        return null;
+    /**
+     * Load file and auto recognition type.
+     *
+     * @param file add watermark file.
+     * @return watermark handler
+     * @see #load(byte[])
+     */
+    public static WatermarkHandler load(File file) throws IOException {
+        return load(new FileInputStream(file));
     }
 
-    public static WatermarkHandler load(InputStream inputStream) {
-        return null;
+    /**
+     * Load file and auto recognition type.
+     *
+     * @param inputStream input stream should be read byte data.
+     * @return watermark handler
+     * @see #load(byte[])
+     */
+    public static WatermarkHandler load(InputStream inputStream) throws IOException {
+        try {
+            int available = inputStream.available();
+            byte[] data = new byte[available];
+            int read = inputStream.read(data);
+            if (read != -1) {
+                throw new LoadFileException("Part of this data is not read.");
+            }
+            return load(data);
+        } finally {
+            inputStream.close();
+        }
     }
 
+    /**
+     * Load file and auto recognition type.
+     *
+     * @param bytes file byte data.
+     * @return The file type corresponding to the watermark processor;
+     */
     public static WatermarkHandler load(byte[] bytes) {
         return null;
     }
---
+++ src/main/java/org/azir/allwatermark/exception/LoadFileException.java
@@ -0,0 +1,12 @@
+package org.azir.allwatermark.exception;
+
+/**
+ * @author Azir
+ * @date 2023/02/19
+ */
+public class LoadFileException extends EasyWatermarkException {
+
+    public LoadFileException(String message) {
+        super(message);
+    }
+}
