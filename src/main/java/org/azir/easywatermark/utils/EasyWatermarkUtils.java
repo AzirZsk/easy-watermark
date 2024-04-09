3fc0c0348b88e76d32385be51d7282430163abc6
Azir <2581866471@qq.com>
2024-04-09 19:25:14 +0800

    feat：add calc utils

---
+++ src/main/java/org/azir/easywatermark/utils/EasyWatermarkUtils.java
@@ -0,0 +1,16 @@
+package org.azir.easywatermark.utils;
+
+/**
+ * @author zhangshukun
+ * @since 2024/04/09
+ */
+public class EasyWatermarkUtils {
+
+    public static double calcRadians(double weight, double height) {
+        return Math.atan(height / weight);
+    }
+
+    public static double calcDegrees(double weight, double height) {
+        return Math.toDegrees(calcRadians(weight, height));
+    }
+}
